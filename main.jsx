e1883136ff177c0fdd2f5e62e27b719456003999
Sennie Im <sennie@MacBookPro.lan>
2025-07-25 01:37:50 +0800

    Fix: use main.jsx for JSX entry, update index.html, remove main.js

--- main.jsx
+++ main.jsx
@@ -2,4 +2,4 @@ import './style.css'
 import App from './src/App.jsx'
 import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('app')).render(<App />)
+createRoot(document.getElementById('app')).render(<App />) 
