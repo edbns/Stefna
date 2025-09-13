bba4829c54f719913997fad038e99f65d38a9125
Sennie Im <sennie@MacBookPro.lan>
2025-09-13 18:46:48 +0800

    Fix import path in system-monitor.ts
    - Change ../_db to ./_db for correct relative path
    - Fixes Netlify build error

--- netlify/functions/system-monitor.ts
+++ netlify/functions/system-monitor.ts
@@ -1,5 +1,5 @@
 import { Handler } from '@netlify/functions'
-import { q, qOne } from '../_db'
+import { q, qOne } from './_db'
 
 // ============================================================================
 // SYSTEM MONITOR - Internal Alert System
