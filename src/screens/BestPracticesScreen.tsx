d863ab511851aeaf175f43762973481776c24681
Sennie Im <sennie@MacBookPro.lan>
2025-09-20 12:14:32 +0800

    Fine-tune copyright text position: change mt-32 to mt-24 for perfect spacing

--- src/screens/BestPracticesScreen.tsx
+++ src/screens/BestPracticesScreen.tsx
@@ -353,7 +353,7 @@ export default function BestPracticesScreen() {
         </div>
 
         {/* Copyright Text */}
-        <p className="text-sm text-white/60 text-center mt-32">
+        <p className="text-sm text-white/60 text-center mt-24">
           © 2025 Stefna. All rights reserved. Unauthorized vibes will be stylishly ignored.
         </p>
       </div>
