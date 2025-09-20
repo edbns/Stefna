791fd5b424e3b3d8320fc8f53e5d572a02522043
Sennie Im <sennie@MacBookPro.lan>
2025-09-20 12:04:49 +0800

    Fix page scroll: add bottom padding to prevent over-scrolling past footer

--- src/screens/BestPracticesScreen.tsx
+++ src/screens/BestPracticesScreen.tsx
@@ -142,7 +142,7 @@ export default function BestPracticesScreen() {
 
 
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black pb-16">
       {/* Simple Back Navigation */}
       <div className="fixed top-6 left-6 z-50">
         <button
