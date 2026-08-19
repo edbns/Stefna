f4c4ed8b4739ce9f340bb919833fa606c0ebcaa1
edbns <souhil@protonmail.ch>
2026-08-19 16:18:18 +0800

    Track Perigo lead before Spotify redirect

---
+++ src/screens/PerigoRedirect.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+export default function PerigoRedirect() {
+  useEffect(() => {
+    window.axon?.('track', 'generate_lead')
+    const target = `https://go.stefna.xyz/Perigo${window.location.search}`
+    const timer = window.setTimeout(() => window.location.replace(target), 1000)
+    return () => window.clearTimeout(timer)
+  }, [])
+  return <div className="min-h-screen bg-black" aria-label="Opening Spotify" />
+}
